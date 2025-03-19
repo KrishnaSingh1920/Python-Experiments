43e305d56233bdf491242c6cbaa259b31e8b8188
Krishna Singh <krishnasinghprojects@gmail.com>
2025-03-19 12:37:02 +0530

    Update Experiment2.py

--- Experiment2.py
+++ Experiment2.py
@@ -1,13 +1,4 @@
-from collections import Counter
-
-filename = input("Enter the filename: ")
-
-with open(filename, "r") as file:
-    words = file.read().lower().split()
-
-word_counts = Counter(words)
-frequent = word_counts.most_common(5)
-
-print("Most frequent words:")
-for word, count in frequent:
-    print(word,":",count)
+import math
+a=int(input("Enter The Number : "))
+result=math.sqrt(a)
+print(f"Square Root : {result}")
